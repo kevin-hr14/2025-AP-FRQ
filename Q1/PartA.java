cbdace7b0ff9be26cad3d3faf65b0829bbef8b8f
kevin-hr14 <houraokevin@gmail.com>
2025-05-12 10:10:42 -0400

    Update PartA.java

--- Q1/PartA.java
+++ Q1/PartA.java
@@ -1,5 +1,11 @@
 //Part A
 
 public int walkDogs(int hour){
-  
+  int c = 0;
+  if (maxDogs >= company.numAvailableDogs(hour)){
+    company.updateDogs(hour,numDogs);
+    return company.numAvailableDogs(hour);
+  }
+  company.updateDogs(hour,nmaxDogs);
+  return company.numAvailableDogs(hour);
 }
