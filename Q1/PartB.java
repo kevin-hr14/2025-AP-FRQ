375392b58fb919028dbe035612a311e9895f3ec5
kevin-hr14 <houraokevin@gmail.com>
2025-05-13 09:37:14 -0400

    Update PartB.java

--- Q1/PartB.java
+++ Q1/PartB.java
@@ -1,3 +1,14 @@
-public int dogWalkShift(int  startHour, int endHour){
-
+public  int dogWalkShift(int startHour, int endHour){
+  int c = 0;
+  
+  for(int i = startHour; i < endHour; i++){
+    c += walkDogs(i)*5;
+    
+    if (walkDogs(i) == maxDogs || (i >= 9 && i <= 17)){
+      c += 3;
+    }
+    
+  }
+  
+  return c;
 }
