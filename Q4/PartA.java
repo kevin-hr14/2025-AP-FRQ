b6cf9feb5235fa44ae8e8f864042887d33ac081f
kevin-hr14 <houraokevin@gmail.com>
2025-05-20 09:43:59 -0400

    Update PartA.java

--- Q4/PartA.java
+++ Q4/PartA.java
@@ -1,1 +1,6 @@
-
+public SumOrSameGame(int rows, int cols) {
+    puzzle = new int[rows][cols];
+    for(int r = 0; r < puzzle.length; r++)
+        for(int c = 0; c < puzzle[0].length; c++)
+            puzzle[r][c] = (int) (Math.random() * 9) +1;
+}
